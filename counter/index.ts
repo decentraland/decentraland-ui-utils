38eb4f52f6ec63d45fbf4d50f1bfe1f196c5ba11
nearnshaw <nearnshaw@decentraland.org>
2020-08-14 16:01:58 -0300

    atlas images

--- counter/index.ts
+++ counter/index.ts
@@ -1,6 +1,5 @@
 import { canvas } from 'decentraland-ui-utils/utils/default-ui-comopnents'
 
-
 /**
  * Displays a number on the center of the UI
  *
@@ -13,41 +12,35 @@ import { canvas } from 'decentraland-ui-utils/utils/default-ui-comopnents'
  */
 
 export class UICounter extends Entity {
-	valueAsNum: number
-	uiText: UIText
-	constructor(
-	  value: number,
-	  xOffset?: number,
-	  yOffset?: number,
-	  color?: Color4,
-	  size?: number
-	) {
-	  super()
-	  this.valueAsNum = value
-	  this.uiText = new UIText(canvas)
-  
-	  this.uiText.value = value.toString()
-  
-	  this.uiText.hAlign = 'right'
-	  this.uiText.vAlign = 'bottom'
-	  this.uiText.positionX = xOffset ? xOffset : 0
-	  this.uiText.positionY = yOffset ? yOffset : 0
-	  this.uiText.fontSize = size ? size : 25
-	  this.uiText.vTextAlign = 'center'
-	  this.uiText.hTextAlign = 'center'
-	  this.uiText.color = color ? color : Color4.FromHexString('#53508F88')
-	}
-  
-	public read(): number {
-	  return this.valueAsNum
-	}
-	public increase(amount?: number): void {
-	  this.valueAsNum += amount ? amount : 1
-	  this.uiText.value = this.valueAsNum.toString()
-	}
-  
-	public decrease(amount?: number): void {
-	  this.valueAsNum -= amount ? amount : 1
-	  this.uiText.value = this.valueAsNum.toString()
-	}
+  valueAsNum: number
+  uiText: UIText
+  constructor(value: number, xOffset?: number, yOffset?: number, color?: Color4, size?: number) {
+    super()
+    this.valueAsNum = value
+    this.uiText = new UIText(canvas)
+
+    this.uiText.value = value.toString()
+
+    this.uiText.hAlign = 'right'
+    this.uiText.vAlign = 'bottom'
+    this.uiText.positionX = xOffset ? xOffset : 0
+    this.uiText.positionY = yOffset ? yOffset : 0
+    this.uiText.fontSize = size ? size : 25
+    this.uiText.vTextAlign = 'center'
+    this.uiText.hTextAlign = 'center'
+    this.uiText.color = color ? color : Color4.FromHexString('#53508F88')
   }
+
+  public read(): number {
+    return this.valueAsNum
+  }
+  public increase(amount?: number): void {
+    this.valueAsNum += amount ? amount : 1
+    this.uiText.value = this.valueAsNum.toString()
+  }
+
+  public decrease(amount?: number): void {
+    this.valueAsNum -= amount ? amount : 1
+    this.uiText.value = this.valueAsNum.toString()
+  }
+}
